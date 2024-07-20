025953a13c4a9ba5baab86a049acc3e27224a53b
xiewi3 <xiewi3@oocl.com>
2024-07-20 22:51:43 +0800

    feat: homework-01 2.ai translator的使用

--- openai-translator/ai_translator/model/openai_model.py
+++ openai-translator/ai_translator/model/openai_model.py
@@ -11,7 +11,7 @@ from openai import OpenAI
 class OpenAIModel(Model):
     def __init__(self, model: str, api_key: str):
         self.model = model
-        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url="https://api.xiaoai.plus/v1")
 
     def make_request(self, prompt):
         attempts = 0
